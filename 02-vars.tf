e5ece4481deb67cab8849bfd166b5aaafbee0f94
shashidharkare26@gmail.com <shashidharkarre26@gmail.com>
2024-04-07 13:07:12 +0530

    commits

--- 02-vars.tf
+++ 02-vars.tf
@@ -9,3 +9,24 @@ output "sample-op" {
 output "sample-op1" {
     value = "Value is ${var.sample}" 
 }
+
+variable "number" {
+  default = 100
+}
+
+output "number" {
+value = var.number  
+}
+
+variable "example-list"{
+    default =  [
+        "Devops",
+        100,
+        false,
+        "shashi"
+    ]
+}
+
+output "example_list" {
+    value = "Welcome to ${var.example-list[0]} Terraform Lab My name is ${var.example-list[3]} "
+}
