b46f706c557f78a0d7196a19848af2f9cd687a1f
shashidharkare26@gmail.com <shashidharkarre26@gmail.com>
2024-04-07 16:03:33 +0530

    commits

--- sample-ec2/ec2.tf
+++ sample-ec2/ec2.tf
@@ -10,27 +10,27 @@ resource "aws_instance" "b49-ec2" {
   }
 } 
 
-resource "aws_security_group" "allow_ssh" {
+resource "aws_security_group" "allow_all" {
   name        = "allow_ssh"
-  description = "Allow SSH inbound traffic and all outbound traffic"
-  vpc_id      = aws_vpc.main.id
+  description = "Allow all inbound traffic"
+
+  ingress {
+    description      = "SSH to VPC"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 
   tags = {
     Name = "allow_ssh"
   }
-}
-
-resource "aws_vpc_security_group_ingress_rule" "allow_ssh_ipv4" {
-  security_group_id = aws_security_group.allow_ssh.id
-  cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 22
-  ip_protocol       = "tcp"
-  to_port           = 22
-}
-
-
-resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
-  security_group_id = aws_security_group.allow_ssh.id
-  cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1" # semantically equivalent to all ports
 }
