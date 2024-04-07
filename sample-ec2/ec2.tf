2529a7910dc7a9913c0fc7045bbb67a31854bd32
shashidharkare26@gmail.com <shashidharkarre26@gmail.com>
2024-04-07 14:11:52 +0530

    commits

---
+++ sample-ec2/ec2.tf
@@ -0,0 +1,12 @@
+resource "aws_instance" "b49-ec2" {
+  ami           = ami-06b125826171f520f
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "my-first-ec2"
+  }
+} 
+
+output "private_ip" {
+value = aws_instance.b49-ec2.private_ip
+}
