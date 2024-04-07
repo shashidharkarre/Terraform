0e45e2f1436ea942891ebdf52c4b986d5e380808
shashidharkare26@gmail.com <shashidharkarre26@gmail.com>
2024-04-07 16:35:27 +0530

    commits

--- tf-remote-state/state.tf
+++ tf-remote-state/state.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "remote-state-file1"
+    bucket = "elasticbeanstalk-us-west-1-471112823068"
     key    = "sample/terraform.tfstate"
     region = "us-west-1"
   }
