72215c37b36166456a21bfa079ff148a47d202e1
shashidharkare26@gmail.com <shashidharkarre26@gmail.com>
2024-04-07 16:01:00 +0530

    commits

--- sample-ec2/ec2.tf
+++ sample-ec2/ec2.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "b49-ec2" {
   ami           = "ami-06b125826171f520f"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
 
   tags = {
     Name = "my-first-ec2"
@@ -9,6 +10,27 @@ resource "aws_instance" "b49-ec2" {
   }
 } 
 
-output "private_ip" {
-value = aws_instance.b49-ec2.private_ip
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic and all outbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  tags = {
+    Name = "allow_ssh"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh_ipv4" {
+  security_group_id = aws_security_group.allow_ssh.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.allow_ssh.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
 }
