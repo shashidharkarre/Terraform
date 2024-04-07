9106976acfa90e2c7481c0c8e9991a814836346c
shashidharkare26@gmail.com <shashidharkarre26@gmail.com>
2024-04-07 20:31:13 +0530

    commits

--- example2/backend.tf
+++ example2/backend.tf
@@ -5,7 +5,3 @@ terraform {
     region = "us-west-1"
   }
 }
-
-provider "aws" {
-region = "us-west-1"  
-}
--- example2/ec2/main.tf
+++ example2/ec2/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "b49-ec2" {
-  ami           = data.aws_ami.base.ami.image_id
+  ami           = data.aws_ami.base-ami.image_id
   instance_type = "t2.micro"
   vpc_security_group_ids = [var.sg]
 } 
